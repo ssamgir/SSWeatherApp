f27ca6ee51f4ac02807b54d3a897be4af356ff20
ssamgir <amitbobade2007@gmail.com>
2016-08-03 21:35:56 +0530

    first commit UI designe started

---
+++ SSWeatherApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SSWeatherApp
+//
+//  Created by Amit Bobade on 03/08/16.
+//  Copyright © 2016 Sandesh Samgir. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ SSWeatherApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SSWeatherApp
+//
+//  Created by Amit Bobade on 03/08/16.
+//  Copyright © 2016 Sandesh Samgir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
